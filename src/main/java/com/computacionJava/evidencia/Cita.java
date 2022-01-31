e43c2b988d86ff831c41850fecd8486f092837de
Alfred <alfred3698@gmail.com>
2022-01-31 14:33:11 -0600

    fixed untracked files

--- src/main/java/com/computacionJava/evidencia/Cita.java
+++ src/main/java/com/computacionJava/evidencia/Cita.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.evidencia;
+package com.computacionJava.evidencia;
 
 import java.util.Date;
 
--- src/main/java/com/computacionJava/evidencia/Main.java
+++ src/main/java/com/computacionJava/evidencia/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.evidencia;
+package com.computacionJava.evidencia;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
